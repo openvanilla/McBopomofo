7fa18543b94811152a4fdfbb14eb1ce9d411123c
Mengjuei <mjhsieh@gmail.com>
2011-11-22 22:35:19 -0800

    filename change

--- Source/Data/bin/build4wlist.bash
+++ Source/Data/bin/build4wlist.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 myFILE=$(mktemp /tmp/XXXXXXX)
 awk '$NF=="big5"' BPMFBase.txt > $myFILE
-awk '{print $1}' $myFILE BPMFMappings.txt |sort| uniq > 1-4w.list
+awk '{print $1}' $myFILE BPMFMappings.txt |sort| uniq > phrase.list
 rm -f $myFILE
--- Source/Data/bin/countphrase.bash
+++ Source/Data/bin/countphrase.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 (
-for myph in $(awk '{print $1}' 1-4w.list); do 
+for myph in $(awk '{print $1}' phrase.list); do 
     echo -n "$myph	"
     awk -F "$myph" '{s=s+NF-1}END{print s}' textpool.utf8
-done ) > /tmp/1-4w.occ
-mv /tmp/1-4w.occ .
+done ) > /tmp/phrase.occ
+mv /tmp/phrase.occ .
