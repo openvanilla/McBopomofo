e60d6e097b9ee40cf5f68adca588cd23b275fa78
Mengjuei <mjhsieh@gmail.com>
2012-03-16 23:52:00 -0700

    revision of Makefile and the phrases

--- Source/Data/bin/buildFreq.bash
+++ Source/Data/bin/buildFreq.bash
@@ -2,10 +2,11 @@
 TOTAL="$(perl -w bin/utf8length.pl < phrase.occ | \
          awk '{s+=($2+2.7^$3)}END{print s}' \
         )"
-perl -w bin/utf8length.pl < phrase.occ | awk -v TOTAL=$TOTAL \
-    ' $2>0{printf("%s %.8f\n",$1,log(( $2*3.7^($3-1))/TOTAL)/log(10))}
-     $2==0{printf("%s %.8f\n",$1,log((0.5*3.7^($3-1))/TOTAL)/log(10))}' \
-    | sed -e 's/inf$/8.0/' > PhraseFreq.txt
+perl -w bin/utf8length.pl < phrase.occ \
+ | awk -v TOTAL=$TOTAL \
+       ' $2>0{printf("%s %.8f\n",$1,log(( $2*3.7^($3-1))/TOTAL)/log(10))}
+        $2==0{printf("%s %.8f\n",$1,log((0.5*3.7^($3-1))/TOTAL)/log(10))}' \
+ | sed -e 's/inf$/8.0/' > PhraseFreq.txt
 #perl -w bin/utf8length.pl < phrase.occ | awk -v TOTAL=$TOTAL \
 #    ' $2>0{printf("%s %.8f\n",$1,log( $2/TOTAL)/log(10))}
 #     $2==0{printf("%s %.8f\n",$1,log(0.5/TOTAL)/log(10))}' \
