d26cd0aeddd0df1a9ecad0308c66d031a6e719d1
Mengjuei <mjhsieh@gmail.com>
2012-09-16 18:18:55 -0700

    typos and data update

--- Source/Data/bin/buildFreq.py
+++ Source/Data/bin/buildFreq.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Mengjuei Hsieh
 import sys
 import math
 
-norm = 0.0
-phrases = {}
+norm      = 0.0
+fscale    = 2.7
+phrases   = {}
 exclusion = {}
 
 if __name__=='__main__':
@@ -37,21 +39,26 @@ if __name__=='__main__':
             exclusion[elements[0]]=elements[1]
     handle.close()
 
+    # eg: if BC and ABC are not related ( BC is meaningless when ABC occurs)
+    #     then count(BC) = count(BC) - count(ABC)
     for k in exclusion:
         v = exclusion[k]
         if k in phrases and v in phrases:
             phrases[k]=phrases[k]-phrases[v]
 
+    # Getting a hint from algorithm of Max-match segmentation
+    # norm = sum ( fscale^(len(phrase)-1) * count(phrase) )
     for k in phrases:
-        norm += 2.7**(len(k)/3-1)*phrases[k]
+        norm += fscale**(len(k)/3-1)*phrases[k]
 
     try:
         handle=open('PhraseFreq.txt',"w")
     except IOError as e:
         print("({})".format(e))
     for k in phrases:
+        # if it's zero count, we treat it as a 0.5 count.
         if phrases[k] < 1:
-            handle.write('%s %.8f\n' % (k, math.log(2.7**(len(k)/3-1)*0.5       /norm,10)))
+            handle.write('%s %.8f\n' % (k, math.log(fscale**(len(k)/3-1)*0.5       /norm,10)))
         else:
-            handle.write('%s %.8f\n' % (k, math.log(2.7**(len(k)/3-1)*phrases[k]/norm,10)))
+            handle.write('%s %.8f\n' % (k, math.log(fscale**(len(k)/3-1)*phrases[k]/norm,10)))
     handle.close()
