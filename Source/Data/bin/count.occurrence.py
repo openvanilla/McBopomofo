a023ec2cc5c89bcd6cac888b35c29b1d8fc53adb
Mengjuei <mjhsieh@gmail.com>
2012-10-13 14:56:43 -0700

    data update, multi-thread counting, Makefile mods.

--- Source/Data/bin/count.occurrence.py
+++ Source/Data/bin/count.occurrence.py
@@ -2,28 +2,50 @@
 # -*- coding: utf-8 -*-
 import sys
 import codecs
+import multiprocessing
+
+__author__ = 'Mengjuei Hsieh'
+__doc__    = """
+   A facility to print a list of counts for substrings / phrases
+   in a text pool file, given a list of phrases."""
+
+# store the content of a text pool file in a global variable
+# not ideal, but should be sufficient.
+bigstring = ''
+try:
+    handle = codecs.open('/Volumes/ramdisk/textpool.02282012', encoding='utf-8', mode='r')
+except IOError as e:
+    print("({})".format(e))
+bigstring=handle.read()
+handle.close()
+
+# return a tuple of (substring, count, status)
+def count_string(substring):
+    if bigstring and substring:
+        return substring, bigstring.count(substring), True
+    return '', 0, False
 
 if __name__=='__main__':
     """
+    bin/count.occurrence.py phrase.list > phrase.occ
     """
-    bigstring = ''
-    try:
-        handle = codecs.open('/Volumes/ramdisk/textpool.02282012', encoding='utf-8', mode='r')
-    except IOError as e:
-        print("({})".format(e))
-    bigstring=handle.read()
-    handle.close()
+    max_cores = multiprocessing.cpu_count()
+    ncores    = max_cores
     try:
         handle = codecs.open(sys.argv[1], encoding='utf-8', mode='r')
     except IOError as e:
         print("({})".format(e))
+    allstrings = []
     while True:
         line = handle.readline()
         if not line: break
         if line[0] == '#': continue
         elements = line.rstrip().split()
-        #myph = elements[0]
-        #mycnt = bigstring.count(myph)
-        outstring = u'%s	%d' % (elements[0],bigstring.count(elements[0]))
-        print outstring.encode('utf-8', 'ignore')
+        allstrings.append(elements[0])
     handle.close()
+    pool = multiprocessing.Pool(ncores)
+    results = pool.map(count_string,allstrings)
+    outputs = [ (phrase, count) for phrase, count, state in results if state is True]
+    for phrase, count in outputs:
+        outstring = u'%s	%d' % (phrase,count)
+        print outstring.encode('utf-8', 'ignore')
