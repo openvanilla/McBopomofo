cdb9c03084559f01f1b7fe82ef340d50d1f4d0fe
Mengjuei <mjhsieh@gmail.com>
2012-10-15 00:30:49 -0700

    new text pool version

--- Source/Data/bin/count.bash
+++ Source/Data/bin/count.bash
@@ -9,7 +9,7 @@ if [ ! -f "${myPATH}/C_count.occ.exe" ]; then
    ( cd $myPATH; make C_count.occ.exe )
 fi
 if [ "${TEXTPOOL}_test" == "_test" ]; then
-   TEXTPOOL=/Volumes/ramdisk/textpool.02282012
+   TEXTPOOL=/Volumes/ramdisk/textpool.10142012
 fi
 if [ -f "$TEXTPOOL" ]; then
    $myPATH/C_count.occ.exe $TEXTPOOL $1
