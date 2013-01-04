65b398d1bf69027d1db7794ccf645d1f33eb6c4f
Mengjuei <mjhsieh@gmail.com>
2013-01-03 22:00:43 -0800

    slight refactoring self-score-test

--- Source/Data/bin/self-score-test.py
+++ Source/Data/bin/self-score-test.py
@@ -9,113 +9,75 @@ XXXXXXX = ['ㄕㄨ˙','ㄌㄧ˙','ㄒㄧ˙','ㄍㄨ˙','ㄊㄞ˙','ㄨㄚ˙',
            'ㄒㄧㄝ˙','ㄌㄡ˙','ㄌㄨ˙', 'ㄐㄧㄝ˙']
 # 'ㄋㄧㄤ˙','ㄉㄧㄢ˙',
 
-# TODO: move section of find highest score from the list to here
-
-# end of TODO
+def segPick(mytest,segcand='',segscore=0):
+    myscore = -9999.99
+    mycand = ''
+    for a,b in phrases[mytest]:
+        if myscore < b:
+            mycand = a
+            myscore = b
+    segcand += mycand
+    segscore += myscore
+    return (segcand, segscore)
 
 def threeCharWalk(testbpmf, (targetP, targetS)):
     bpmfinput = testbpmf.split('-')
     candidate = []
     i = 0
-    #
+    #123
     mytest = '-'.join(bpmfinput[0:3])
     if mytest in phrases:
         segcand = ''
         segscore = 0
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #1-2-3
     if bpmfinput[0] not in XXXXXXX and bpmfinput[1] not in XXXXXXX and bpmfinput[2] not in XXXXXXX:
         segcand = ''
         segscore = 0
         for mybpmf in bpmfinput:
-            myscore = -9999.99
-            mycand = ''
             if mybpmf not in phrases:
                 print mybpmf
                 print testbpmf
                 sys.exit(0)
-            for a,b in phrases[mybpmf]:
-                if myscore < b:
-                    mycand = a
-                    myscore = b
-            segcand += mycand
-            segscore += myscore   
+            (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #1-23
     if bpmfinput[0] not in XXXXXXX and '-'.join(bpmfinput[1:3]) in phrases:
         segcand = ''
         segscore = 0
         mybpmf = bpmfinput[0]
-        myscore = -9999.99
-        mycand = ''
         if mybpmf not in phrases:
             print mybpmf
             print testbpmf
             sys.exit(0)
-        for a,b in phrases[mybpmf]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         mytest = '-'.join(bpmfinput[1:3])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #12-3
     if bpmfinput[2] not in XXXXXXX and '-'.join(bpmfinput[0:2]) in phrases:
         segcand = ''
         segscore = 0
         mytest = '-'.join(bpmfinput[0:2])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         mybpmf = bpmfinput[2]
-        myscore = -9999.99
-        mycand = ''
         if mybpmf not in phrases:
             print mybpmf
             print testbpmf
             sys.exit(0)
-        for a,b in phrases[mybpmf]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+
     candidate.sort(key=lambda x: x[1], reverse=True)
-    #for i in range(6):
-    #    print '%s %f' % candidate[i]
-    #sys.exit(0)
     if (targetP, targetS) == candidate[0]:
         pass
     else:
@@ -126,206 +88,98 @@ def fourCharWalk(testbpmf, (targetP, targetS)):
     bpmfinput = testbpmf.split('-')
     candidate = []
     i = 0
-    #
+    #1234
     mytest = '-'.join(bpmfinput[0:4])
     if mytest in phrases:
         segcand = ''
         segscore = 0
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #1-2-3-4
     if bpmfinput[0] not in XXXXXXX and bpmfinput[1] not in XXXXXXX and bpmfinput[2] not in XXXXXXX and bpmfinput[3] not in XXXXXXX:
         segcand = ''
         segscore = 0
         for mybpmf in bpmfinput:
-            myscore = -9999.99
-            mycand = ''
             if mybpmf not in phrases:
                 print mybpmf
                 print testbpmf
                 sys.exit(0)
-            for a,b in phrases[mybpmf]:
-                if myscore < b:
-                    mycand = a
-                    myscore = b
-            segcand += mycand
-            segscore += myscore   
+            (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #12-3-4
     mytest = '-'.join(bpmfinput[0:2])
     if bpmfinput[2] not in XXXXXXX and bpmfinput[3] not in XXXXXXX and mytest in phrases:
         segcand = ''
         segscore = 0
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         for mybpmf in bpmfinput[2:4]:
-            myscore = -9999.99
-            mycand = ''
-            for a,b in phrases[mybpmf]:
-                if myscore < b:
-                    mycand = a
-                    myscore = b
-            segcand += mycand
-            segscore += myscore   
+            (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
-    if bpmfinput[0] not in XXXXXXX and bpmfinput[3] and '-'.join(bpmfinput[1:3]) in phrases:
+        #print '%s %f' % candidate[i]
+    #1-23-4
+    if bpmfinput[0] not in XXXXXXX and bpmfinput[3] not in XXXXXXX and '-'.join(bpmfinput[1:3]) in phrases:
         segcand = ''
         segscore = 0
         for mybpmf in bpmfinput[0:1]:
-            myscore = -9999.99
-            mycand = ''
-            for a,b in phrases[mybpmf]:
-                if myscore < b:
-                    mycand = a
-                    myscore = b
-            segcand += mycand
-            segscore += myscore   
-        myscore = -9999.99
-        mycand = ''
+            (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         mytest = '-'.join(bpmfinput[1:3])
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         for mybpmf in bpmfinput[3:4]:
-            myscore = -9999.99
-            mycand = ''
-            for a,b in phrases[mybpmf]:
-                if myscore < b:
-                    mycand = a
-                    myscore = b
-            segcand += mycand
-            segscore += myscore   
+            (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
-    segcand = ''
-    segscore = 0
+        #print '%s %f' % candidate[i]
+    #12-34
     if '-'.join(bpmfinput[0:2]) in phrases and '-'.join(bpmfinput[2:4]) in phrases:
+        segcand = ''
+        segscore = 0
         mytest = '-'.join(bpmfinput[0:2])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         mytest = '-'.join(bpmfinput[2:4])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #1-2-34
     if bpmfinput[0] not in XXXXXXX and bpmfinput[1] not in XXXXXXX and '-'.join(bpmfinput[2:4]) in phrases:
         segcand = ''
         segscore = 0
         for mybpmf in bpmfinput[0:2]:
-            myscore = -9999.99
-            mycand = ''
-            for a,b in phrases[mybpmf]:
-                if myscore < b:
-                    mycand = a
-                    myscore = b
-            segcand += mycand
-            segscore += myscore   
+            (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         mytest = '-'.join(bpmfinput[2:4])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #1-234
     if bpmfinput[0] not in XXXXXXX and '-'.join(bpmfinput[1:4]) in phrases:
         segcand = ''
         segscore = 0
         mybpmf = bpmfinput[0]
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mybpmf]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         mytest = '-'.join(bpmfinput[1:4])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+    #123-4
     if bpmfinput[3] not in XXXXXXX and '-'.join(bpmfinput[0:3]) in phrases:
         segcand = ''
         segscore = 0
         mytest = '-'.join(bpmfinput[0:3])
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mytest]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mytest,segcand,segscore)
         mybpmf = bpmfinput[3]
-        myscore = -9999.99
-        mycand = ''
-        for a,b in phrases[mybpmf]:
-            if myscore < b:
-                mycand = a
-                myscore = b
-        segcand += mycand
-        segscore += myscore   
+        (segcand, segscore) = segPick(mybpmf,segcand,segscore)
         candidate.append((segcand, segscore))
-        #print '%s %f' % candidate[i]
         i += 1
-    #
+        #print '%s %f' % candidate[i]
+
     candidate.sort(key=lambda x: x[1], reverse=True)
-    #for i in range(6):
-    #    print '%s %f' % candidate[i]
-    #sys.exit(0)
     if (targetP, targetS) == candidate[0]:
         pass
     else:
@@ -348,13 +202,11 @@ if __name__=='__main__':
             phrases[elements[1]] = []
         phrases[elements[1]].append((elements[0], float(elements[2])))
     handle.close()
-    #testbpmf = 'ㄍㄨㄛˊ-ㄐㄧㄚ-ㄍㄨㄥ-ㄩㄢˊ'
+    #mybpmf = 'ㄍㄨㄛˊ-ㄐㄧㄚ-ㄍㄨㄥ-ㄩㄢˊ'
     for mybpmf in phrases:
         if len(phrases[mybpmf]) > 1:
             phrases[mybpmf].sort(key=lambda x: x[1], reverse=True)
         if len(mybpmf.split('-')) is 3:
             threeCharWalk(mybpmf,phrases[mybpmf][0])
-            #print mybpmf
         if len(mybpmf.split('-')) is 4:
             fourCharWalk(mybpmf,phrases[mybpmf][0])
-    #fourCharWalk(testbpmf)
