e9c965382e615607781186937a0b7a86c02fbf03
Mengjuei <mjhsieh@gmail.com>
2014-08-23 18:52:57 -0700

    enable the real building

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-xcodebuild -target McBopomofo -configuration Debug clean
-xcodebuild -target McBopomofo -configuration Debug clean
+xcodebuild -target McBopomofo -configuration Release clean
+xcodebuild -target McBopomofo -configuration Release
