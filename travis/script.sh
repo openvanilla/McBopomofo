61219295de01c527dae28c09b439c208e50b63c3
zonble <zonble@gmail.com>
2021-11-12 00:56:11 +0800

    Updates CI settings.

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
 xcodebuild -scheme McBopomofo -configuration Release clean
-xcodebuild -scheme McBopomofo -configuration Release
+xcodebuild -scheme McBopomofo -configuration Release build
+xcodebuild -scheme McBopomofoInstaller -configuration Release clean
+xcodebuild -scheme McBopomofoInstaller -configuration Release build
