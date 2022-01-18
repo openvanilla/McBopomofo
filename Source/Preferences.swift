9d609695e19a798ff6b55dc119cb07d9f5d61cfc
Lukhnos Liu <lukhnos@lukhnos.org>
2022-01-18 14:08:44 -0800

    Update copyright headers (fixes #213)

--- Source/Preferences.swift
+++ Source/Preferences.swift
@@ -1,14 +1,4 @@
-//
-// Preferences.swift
-//
-// Copyright (c) 2011 The McBopomofo Project.
-//
-// Contributors:
-//     Mengjuei Hsieh (@mjhsieh)
-//     Weizhong Yang (@zonble)
-//
-// Based on the Syrup Project and the Formosana Library
-// by Lukhnos Liu (@lukhnos).
+// Copyright (c) 2022 and onwards The McBopomofo Authors.
 //
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
@@ -30,7 +20,6 @@
 // WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
-//
 
 import Cocoa
 
