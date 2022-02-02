7c9e9cd9b76455a7db2813ffb759d93854a0fc40
zonble <zonble@gmail.com>
2022-02-03 01:07:54 +0800

    Bug fixes on the marking state.
    When the length of the range become to 0, the controller should go back
    to inputting state.

--- Source/KeyHandler.mm
+++ Source/KeyHandler.mm
@@ -852,7 +852,13 @@ static NSString *const kGraphVizOutputfile = @"/tmp/McBopomofo-visualization.dot
             index = [state.composingBuffer previousUtf16PositionFor:index];
             InputStateMarking *marking = [[InputStateMarking alloc] initWithComposingBuffer:state.composingBuffer cursorIndex:state.cursorIndex markerIndex:index readings:state.readings];
             marking.tooltipForInputting = state.tooltipForInputting;
-            stateCallback(marking);
+
+            if (marking.markedRange.length == 0) {
+                InputState *inputting = [marking convertToInputting];
+                stateCallback(inputting);
+            } else {
+                stateCallback(marking);
+            }
         } else {
             errorCallback();
             stateCallback(state);
@@ -868,6 +874,12 @@ static NSString *const kGraphVizOutputfile = @"/tmp/McBopomofo-visualization.dot
             index = [state.composingBuffer nextUtf16PositionFor:index];
             InputStateMarking *marking = [[InputStateMarking alloc] initWithComposingBuffer:state.composingBuffer cursorIndex:state.cursorIndex markerIndex:index readings:state.readings];
             marking.tooltipForInputting = state.tooltipForInputting;
+            if (marking.markedRange.length == 0) {
+                InputState *inputting = [marking convertToInputting];
+                stateCallback(inputting);
+            } else {
+                stateCallback(marking);
+            }
             stateCallback(marking);
         } else {
             errorCallback();
