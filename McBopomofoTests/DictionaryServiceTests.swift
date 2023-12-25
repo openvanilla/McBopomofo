88339a9ecc15b272efbc0f629e91727a66091943
zonble <zonble@gmail.com>
2023-12-26 00:38:18 +0800

    Fixes tests.

--- McBopomofoTests/DictionaryServiceTests.swift
+++ McBopomofoTests/DictionaryServiceTests.swift
@@ -4,15 +4,24 @@ import XCTest
 final class DictionaryServiceTests: XCTestCase {
 
     func testSpeak() {
-        let result = DictionaryServices.shared.lookUp(phrase: "你", withServiceAtIndex: 0)
+        let result = DictionaryServices.shared.lookUp(phrase: "你", withServiceAtIndex: 0, state: InputState.Empty()) { _ in
+
+        }
         XCTAssertTrue(result)
     }
 
     func testDictionaryService() {
         let count = DictionaryServices.shared.services.count
         for index in 0..<count {
-            let result = DictionaryServices.shared.lookUp(phrase: "你", withServiceAtIndex: index)
-            XCTAssertTrue(result)
+            var callbackCalled = false
+            let choosing = InputState.ChoosingCandidate(composingBuffer: "hi", cursorIndex: 0, candidates: [InputState.Candidate(reading: "", value: "", displayText: "")], useVerticalMode: false)
+            let selecting =  InputState.SelectingDictionaryService(previousState: choosing, selectedString: "你", selectedIndex: 0)
+            let result = DictionaryServices.shared.lookUp(phrase: "你", withServiceAtIndex: index, state: selecting) { _ in
+                callbackCalled = true
+            }
+            if !callbackCalled {
+                XCTAssertTrue(result)
+            }
         }
     }
 
