4e45187cca19f15084d9f5c1d65b83feabefbd4d
zonble <zonble@gmail.com>
2024-02-23 14:14:18 +0800

    Fixes the tokenizer bugs.

--- Source/ServiceProvider.swift
+++ Source/ServiceProvider.swift
@@ -25,17 +25,12 @@ import AppKit
 import BopomofoBraille
 import OpenCCBridge
 
-@objc protocol ServiceProviderDelegate: NSObjectProtocol {
-    @objc(serviceProvider:didRequestInsertReading:)
-    func serviceProvider(_ provider: ServiceProvider, didRequestInsertReading: String)
-    @objc(serviceProviderDidRequestCommitting:)
-    func serviceProvider(didRequestCommitting provider: ServiceProvider) -> String
-    @objc(serviceProviderDidRequestReset:)
-    func serviceProvider(didRequestReset provider: ServiceProvider)
-}
-
 class ServiceProvider: NSObject {
-    weak var delegate: ServiceProviderDelegate?
+    weak var delegate: ServiceProviderDelegate? {
+        didSet {
+            delegate?.serviceProvider(didRequestReset: self)
+        }
+    }
 
     func extractReading(from firstWord: String) -> String {
         var matches: [String] = []
@@ -93,97 +88,164 @@ class ServiceProvider: NSObject {
         LanguageModelManager.writeUserPhrase("\(firstWord) \(reading)")
         (NSApp.delegate as? AppDelegate)?.openUserPhrases(self)
     }
+}
+
+@objc protocol ServiceProviderDelegate: NSObjectProtocol {
+    @objc(serviceProvider:didRequestInsertReading:)
+    func serviceProvider(_ provider: ServiceProvider, didRequestInsertReading: String)
+    @objc(serviceProviderDidRequestCommitting:)
+    func serviceProvider(didRequestCommitting provider: ServiceProvider) -> String
+    @objc(serviceProviderDidRequestReset:)
+    func serviceProvider(didRequestReset provider: ServiceProvider)
+}
+
+// MARK: -
+
+private let kMaxLength = 3000
+
+extension ServiceProvider {
 
     // MARK: -
 
+    /// Use Apple's tokenizer to tokenize the input string.
+    func tokenize(string: String) -> [String] {
+        let cfString = string as CFString
+        let tokenizer = CFStringTokenizerCreate(
+            nil, cfString, CFRange(location: 0, length: CFStringGetLength(cfString)), 0, nil)
+        var readHead = 0
+        var output: [String] = []
+        while readHead < CFStringGetLength(cfString) {
+            _ = CFStringTokenizerAdvanceToNextToken(tokenizer)
+            let range = CFStringTokenizerGetCurrentTokenRange(tokenizer)
+            if range.location == kCFNotFound {
+                if let subString = CFStringCreateWithSubstring(
+                    nil, cfString, CFRangeMake(readHead, 1))
+                {
+                    output.append(subString as String)
+                }
+                readHead += 1
+                continue
+            }
+
+            if range.location > readHead {
+                if let subString = CFStringCreateWithSubstring(
+                    nil, cfString, CFRange(location: readHead, length: range.location - readHead))
+                {
+                    output.append(subString as String)
+                }
+            }
+            if let subString = CFStringCreateWithSubstring(nil, cfString, range) {
+                output.append(subString as String)
+            }
+            readHead = range.location + range.length
+        }
+        return output
+    }
+
+    func process(
+        string: String,
+        readingFoundCallback: (String, String) -> String,
+        readingNotFoundCallback: (String) -> String
+    ) -> String {
+        var output = ""
+        let tokens = tokenize(string: string)
+        for token in tokens {
+            if let reading = LanguageModelManager.reading(for: token) {
+                if reading.isEmpty == false && reading.starts(with: "_") == false {
+                    let readings = reading.components(separatedBy: "-")
+                    if readings.count == token.count {
+                        for (index, c) in token.enumerated() {
+                            output += readingFoundCallback(String(c), readings[index])
+                        }
+                        continue
+                    }
+                }
+            }
+            for c in token {
+                if let reading = LanguageModelManager.reading(for: String(c)) {
+                    if reading.isEmpty == false && reading.starts(with: "_") == false {
+                        output += readingFoundCallback(String(c), reading)
+                    } else {
+                        output += readingNotFoundCallback("\(c)")
+                    }
+                } else {
+                    output += readingNotFoundCallback("\(c)")
+                }
+            }
+        }
+        return output
+    }
+
+    func addReading(string: String) -> String {
+        process(string: string) {
+            "\($0)(\($1))"
+        } readingNotFoundCallback: {
+            $0
+        }
+    }
+
     @objc func addReading(_ pasteboard: NSPasteboard, userData: String?, error: NSErrorPointer) {
-        guard let string = pasteboard.string(forType: .string)
+        guard let string = pasteboard.string(forType: .string), string.count < kMaxLength,
+            let converted = OpenCCBridge.shared.convertTraditional(String(string))
         else {
             return
         }
-        var output = ""
-        for c in string {
-            output += String(c)
-            if let converted = OpenCCBridge.shared.convertTraditional(String(c)),
-                let reading = LanguageModelManager.reading(for: converted)
-            {
-                if reading.isEmpty == false && reading.starts(with: "_") == false {
-                    output += "(\(reading))"
-                }
-            }
+        let output = addReading(string: converted)
+        if output.isEmpty {
+            return
         }
         pasteboard.clearContents()
         pasteboard.declareTypes([.string], owner: nil)
         pasteboard.writeObjects([output as NSString])
     }
-}
 
-extension ServiceProvider {
     func convertToReadings(string: String) -> String {
-        var output = ""
-        for c in string {
-            if let converted = OpenCCBridge.shared.convertTraditional(String(c)),
-               let reading = LanguageModelManager.reading(for: converted)
-            {
-                if reading.isEmpty == false && reading.starts(with: "_") == false {
-                    output += reading
-                } else {
-                    output += String(c)
-                }
-            } else {
-                output += String(c)
-            }
+        process(string: string) {
+            $1
+        } readingNotFoundCallback: {
+            $0
         }
-        return output
     }
 
     @objc func convertToReadings(
         _ pasteboard: NSPasteboard, userData: String?, error: NSErrorPointer
     ) {
-        guard let string = pasteboard.string(forType: .string)
+        guard let string = pasteboard.string(forType: .string), string.count < kMaxLength
         else {
             return
         }
-        var output = convertToReadings(string: string)
+        let output = convertToReadings(string: string)
         pasteboard.clearContents()
         pasteboard.declareTypes([.string], owner: nil)
         pasteboard.writeObjects([output as NSString])
     }
 
+    // MARK: - Braille
+
     func convertToBraille(string: String) -> String {
-        var output = ""
-        for c in string {
-            if let converted = OpenCCBridge.shared.convertTraditional(String(c)),
-               let reading = LanguageModelManager.reading(for: converted)
-            {
-                if reading.isEmpty == false && reading.starts(with: "_") == false {
-                    output += BopomofoBrailleConverter.convert(bopomofo: reading)
-                } else {
-                    output += BopomofoBrailleConverter.convert(bopomofo: String(c))
-                }
-            } else {
-                output += BopomofoBrailleConverter.convert(bopomofo: String(c))
-            }
+        process(string: string) {
+            BopomofoBrailleConverter.convert(bopomofo: $1)
+        } readingNotFoundCallback: {
+            BopomofoBrailleConverter.convert(bopomofo: $0)
         }
-        return output
     }
 
     @objc func convertToBraille(
         _ pasteboard: NSPasteboard, userData: String?, error: NSErrorPointer
     ) {
-        guard let string = pasteboard.string(forType: .string)
+        guard let string = pasteboard.string(forType: .string), string.count < kMaxLength
         else {
             return
         }
-        var output = convertToBraille(string: string)
+        let output = convertToBraille(string: string)
         pasteboard.clearContents()
         pasteboard.declareTypes([.string], owner: nil)
         pasteboard.writeObjects([output as NSString])
     }
 
     func convertBrailleToChineseText(string: String) -> String {
+        delegate?.serviceProvider(didRequestReset: self)
         var output = ""
-        let test = BopomofoBrailleConverter.convert(braille: string)
         let tokens = BopomofoBrailleConverter.convert(brailleToTokens: string)
 
         for token in tokens {
@@ -212,7 +274,11 @@ extension ServiceProvider {
         else {
             return
         }
-        var output = convertBrailleToChineseText(string: string)
+        let output = convertBrailleToChineseText(string: string)
+        if output.isEmpty {
+            return
+        }
+
         pasteboard.clearContents()
         pasteboard.declareTypes([.string], owner: nil)
         pasteboard.writeObjects([output as NSString])
