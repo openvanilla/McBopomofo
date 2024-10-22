84d6871777434ab8ff9fd6cf52e15bb2bfc906f7
zonble <zonble@gmail.com>
2024-10-22 19:21:57 +0800

    Fixes a duplicated line.

--- McBopomofoTests/KeyHandlerBopomofoTests.swift
+++ McBopomofoTests/KeyHandlerBopomofoTests.swift
@@ -1,5 +1,4 @@
 // Copyright (c) 2022 and onwards The McBopomofo Authors.
-// Copyright (c) 2022 and onwards The McBopomofo Authors.
 //
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
